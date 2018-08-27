382606b30a24fb245048f836d4eae571b414a5d7
user <user@DESKTOP-7EQF3M8>
2018-08-27 14:32:45 +0900

    수정본

---
+++ Massenger/src/com/mini/exception/InputStreamException.java
@@ -0,0 +1,16 @@
+package com.mini.exception;
+
+import java.io.IOException;
+
+public class InputStreamException extends Exception{
+	public static String InputStreamException() {
+		String error = "인풋에러입니다.";
+		return error;
+	}
+	
+	public static String 
+	@Override
+	public void printStackTrace() {
+		
+	}
+}
