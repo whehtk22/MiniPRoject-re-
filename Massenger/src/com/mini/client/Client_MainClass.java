8fb6c928a01ed0c80120153d7f630d2bcb745ced
user <user@DESKTOP-7EQF3M8>
2018-08-30 15:21:21 +0900

    채팅 완료

--- Massenger/src/com/mini/client/Client_MainClass.java
+++ Massenger/src/com/mini/client/Client_MainClass.java
@@ -4,7 +4,7 @@ import com.mini.gui.*;
 
 public class Client_MainClass {
 	public static void main(String[] args) {
-		Client_cennection cm = new Client_cennection(41000, "192.168.0.3");
+		Client_cennection cm = new Client_cennection(41000, "localhost");
 		Lobby lb = new Lobby(cm);
 //		Gui3 gui = new Gui3();
 	}
